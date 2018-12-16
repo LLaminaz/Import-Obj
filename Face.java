a252bd92e15f477763f907d592d9a7526d44f4e3
LLaminaz <43671373+LLaminaz@users.noreply.github.com>
2018-12-16 18:16:51 +0100

    Initial commit

---
+++ Face.java
@@ -0,0 +1,27 @@
+
+//package importobj;
+import javax.vecmath.*;
+import java.util.*;
+
+/*
+Cos'è una face?
+è un oggetto che collega i vertici con i normal per poter di fatto creare i triangoli.
+Inoltre ha anche la possibilità di collegarci la texture ma che in questo caso non ci interessa.
+
+Cos'è un Vector3f?
+è di fatto la rappresentazione in java di un vettore 3-dimensionale.
+in particolare in questo caso ci riferiamo a un vettore 3-dimensionale di float.
+*/
+
+public class Face
+{
+  Vector3f vertex=new Vector3f(); //vertice
+  Vector3f normal=new Vector3f(); //normal
+
+  //metodo costruttore semplice
+  public Face(Vector3f vertex_par,Vector3f normal_par)
+  {
+    vertex=vertex_par;
+    normal=nromal_par;
+  }
+}
