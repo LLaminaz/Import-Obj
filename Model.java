8840bd4670ac2cd1dae2b4ebf3a955adebe6d15d
LLaminaz <43671373+LLaminaz@users.noreply.github.com>
2018-12-17 09:04:49 +0100

    Initial Commit

---
+++ Model.java
@@ -0,0 +1,22 @@
+
+//package importobj;
+import javax.vecmath.*;
+
+public class Model
+{
+  ArrayList<Vector3f> vertex=new ArrayList();
+  ArrayList<Vector3f> normal=new ArrayList();
+  ArrayList<Face> faces=new ArrayList();
+
+  public Model()
+  {
+    
+  }
+
+  public Model(ArrayList<Vector3f> vertex_par, ArrayList<Vector3f> normal_par, ArrayList<Vector3f> faces_par)
+  {
+    vertex=vertex_par;
+    normal=normal_par;
+    faces=faces_par;
+  }
+}
---
+++ ObjLoader.java
@@ -0,0 +1,14 @@
+
+//package importobj;
+
+public class ObjLoader
+{
+  public Model Loader(File f)
+  {
+    Model m=new Model();
+    BufferedReader in=new BufferedReader(new FileReader("*.obj"));
+    
+
+    return m;
+  }
+}
