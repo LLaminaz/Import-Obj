a439a8f0c64993ff05c47afcf0fb0e3326a2dda8
LLaminaz <43671373+LLaminaz@users.noreply.github.com>
2018-12-18 12:12:16 +0100

    Commit1

--- ObjLoader.java
+++ ObjLoader.java
@@ -1,5 +1,6 @@
 
 //package importobj;
+import org.lwjgl.*;
 
 public class ObjLoader
 {
@@ -7,7 +8,37 @@ public class ObjLoader
   {
     Model m=new Model();
     BufferedReader in=new BufferedReader(new FileReader("*.obj"));
-    
+    String app=new String();
+    float x,y,z;
+    while((app = in.readLine())!=NULL)
+    {
+      if(app.split(" ")[0]=="v")
+      {
+        x=Float.parseFloat(app.split(" ")[1]);
+        y=Float.parseFloat(app.split(" ")[2]);
+        z=Float.parseFloat(app.split(" ")[3]);
+        m.vertex.add(new Vector3f(x,y,z));
+      }
+      else if(app.split(" ")[0]=="vn")
+      {
+        x=Float.parseFloat(app.split(" ")[1]);
+        y=Float.parseFloat(app.split(" ")[2]);
+        z=Float.parseFloat(app.split(" ")[3]);
+        m.normal.add(new Vector3f(x,y,z));
+      }
+      else if(app.split(" ")[0]=="f")
+      {
+        x=Float.parseFloat(app.split(" ")[1].split("/")[0]);
+        y=Float.parseFloat(app.split(" ")[2].split("/")[0]);
+        z=Float.parseFloat(app.split(" ")[3].split("/")[0]);
+        Vector3f app1=new Vector3f(x,y,z);
+        x=Float.parseFloat(app.split(" ")[1].split("/")[2]);
+        y=Float.parseFloat(app.split(" ")[2].split("/")[2]);
+        z=Float.parseFloat(app.split(" ")[3].split("/")[2]);
+        Vector3f app2=new Vector3f(x,y,z);
+        m.faces.add(new Face(app1,app2));
+      }
+    }
 
     return m;
   }
